6385a85cf7567b8b74cb9cfb8f6f8097c2431ea0
Ian Kruger <107899965+ianckruger@users.noreply.github.com>
2024-02-26 00:07:08 +0000

    updating info

--- src/User.java
+++ src/User.java
@@ -1,11 +1,30 @@
-
+import java.util.UUID;
 
 public abstract class User {
     private String firstName;
     private String lastName;
-    private String userID;
+    private UUID userID;
     private String password;
     private String userType;
 
 
+    // new account
+    public User(String firstName, String lastName, String password, String userType) {
+        this.userID = UUID.randomUUID();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.userType = userType;
+
+    }
+
+    //existing account
+    public User(UUID userID, String firstName, String lastName, String password, String userType) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userID = userID;
+        this.password = password;
+        this.userType = userType;
+
+    }
 }
