d8ef128f7bb1036266fcb6bc1293ab628fe54771
Ian Kruger <107899965+ianckruger@users.noreply.github.com>
2024-02-27 23:22:35 +0000

    Updating #30 and #23

--- src/backend/Advisor.java
+++ src/backend/Advisor.java
@@ -1,4 +1,23 @@
-package backend;
-public class Advisor {
-    
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Advisor extends User {
+    private ArrayList<User> students;
+    private User advising;
+
+    public Advisor(String firstName, String lastName, String password, String userType, ArrayList<User> students, User advising) {
+        super(firstName, lastName, password, userType);
+        this.advising = advising;
+        this.students = students;
+
+        
+    }
+
+    public Advisor(UUID userID, String firstName, String lastName, String password, String userType, ArrayList<User> students, User advising) {
+        super(userID, firstName, lastName, password, userType);
+        this.advising = advising;
+        this.students = students;
+
+        
+    }
 }
