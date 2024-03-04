04e42fdab1edfb50fce74025317fa23c827a0805
Victor Nguyen <vicvngu@gmail.com>
2024-03-04 15:08:15 -0500

    data loader for roadmap #16

---
+++ src/backend/RoadmapList.java
@@ -0,0 +1,33 @@
+package backend;
+
+import java.util.ArrayList;
+
+
+public class RoadmapList {
+
+    private static RoadmapList roadmaps;
+    private ArrayList<Roadmap> roadmapList;
+    private Roadmap activeRoadmap;
+
+    private RoadmapList() {
+        this.activeRoadmap = null;
+        // roadmapList = DataLoader.readRoadmaps();
+    }
+
+    public static RoadmapList getInstance() {
+        if(roadmaps == null) {
+            roadmaps = new RoadmapList();
+            //roadmaps.roadmapList = DataLoader.readRoadmaps();
+        }
+        return roadmaps;
+    }
+
+    public ArrayList<Roadmap> getRoadmaps() {
+        return roadmapList;
+    } 
+
+    public void addRoadmap(Roadmap roadmap) {
+        roadmapList.add(roadmap);
+    }
+
+}
