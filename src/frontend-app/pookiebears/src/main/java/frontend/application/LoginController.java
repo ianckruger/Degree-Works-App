af3a7a2e68b391159fbb9b674383f413989ed657
bnguyen0427 <brandonnguyen0427@gmail.com>
2024-04-10 16:45:25 -0400

    running login

---
+++ src/frontend-app/pookiebears/src/main/java/frontend/application/LoginController.java
@@ -0,0 +1,15 @@
+package frontend.application;
+
+import javafx.fxml.Initializable;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+
+public class LoginController implements Initializable{
+    
+}
