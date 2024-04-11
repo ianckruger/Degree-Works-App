6fab106da53f4029bbe2fa9b1289e44cd974c6dd
bnguyen0427 <brandonnguyen0427@gmail.com>
2024-04-11 13:04:30 -0400

    fixing signup

--- src/frontend-app/pookiebears/src/main/java/frontend/application/SignUpController.java
+++ src/frontend-app/pookiebears/src/main/java/frontend/application/SignUpController.java
@@ -1,10 +1,26 @@
 package frontend.application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import frontend.application.App;
+import backend.*;
 
-public class SignUpController {
+
+
+public class SignUpController implements Initializable {
+
+    @FXML
+    private MenuButton menuButton;
+
     @FXML
     private MenuItem advisor_type;
 
@@ -23,6 +39,43 @@ public class SignUpController {
     @FXML
     private TextField txt_username;
 
+    @FXML
+    private Label lbl_error;
+
+    @FXML
+    void handleMenuItem(ActionEvent event) throws Exception{
+        MenuItem menuItem = (MenuItem) event.getSource();
+        String selectedItem = menuItem.getText();
+        menuButton.setText(selectedItem);
+
+    }
+
+    @FXML
+    void SignUpClicked(MouseEvent event) throws Exception{
+
+        String username = txt_username.getText();
+        String password = txt_password.getText();
+        String firstName = txt_firstname.getText();
+        String lastName = txt_lastname.getText();
+        String userType = menuButton.getText();
+        
+
+        RoadmapApplication application = RoadmapApplication.getInstance();
+
+        if (!application.register(username, firstName, lastName, password, userType))  {
+            lbl_error.setText("Account couldn't be created !");
+            return;
+
+        }
+        App.setRoot("login");
+
+
+    }
+
+
+     @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
 
     
 }
