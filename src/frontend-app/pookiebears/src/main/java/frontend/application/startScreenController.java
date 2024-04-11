984c65f311a528b1661ca648ff3c4c427dcfe59b
bnguyen0427 <brandonnguyen0427@gmail.com>
2024-04-11 11:07:40 -0400

    start screen

--- src/frontend-app/pookiebears/src/main/java/frontend/application/startScreenController.java
+++ src/frontend-app/pookiebears/src/main/java/frontend/application/startScreenController.java
@@ -7,6 +7,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.event.ActionEvent;
 import frontend.application.App;
 import backend.*;
 
