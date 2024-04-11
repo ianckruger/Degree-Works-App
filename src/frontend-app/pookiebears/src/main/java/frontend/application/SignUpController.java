d20e1ec9ca4bb7eb85be7085b7a7efa7cd5dc70e
bnguyen0427 <brandonnguyen0427@gmail.com>
2024-04-11 11:30:09 -0400

    creating sign up controller

---
+++ src/frontend-app/pookiebears/src/main/java/frontend/application/SignUpController.java
@@ -0,0 +1,28 @@
+package frontend.application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+    @FXML
+    private MenuItem advisor_type;
+
+    @FXML
+    private MenuItem student_type;
+
+    @FXML
+    private TextField txt_firstname;
+
+    @FXML
+    private TextField txt_lastname;
+
+    @FXML
+    private TextField txt_password;
+
+    @FXML
+    private TextField txt_username;
+
+
+    
+}
