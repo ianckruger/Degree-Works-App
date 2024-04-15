c4cd1c76259bf31b847d25640bc58a7a3abb02e4
bnguyen0427 <brandonnguyen0427@gmail.com>
2024-04-15 10:46:50 -0400

    fixing

---
+++ src/frontend-app/pookiebears/src/main/java/frontend/application/studentProfileController.java
@@ -0,0 +1,24 @@
+package frontend.application;
+
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import frontend.application.App;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import backend.*;
+
+public class studentProfileController implements Initializable {
+    @FXML
+    void academicClicked(MouseEvent event) throws IOException {
+        App.setRoot("studentAcademic");
+
+    }
+
+     @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+
+}
--- src/frontend-app/pookiebears/src/main/java/module-info.java
+++ src/frontend-app/pookiebears/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module frontend.application {
     requires javafx.controls;
     requires javafx.fxml;
     requires json.simple;
+    requires javafx.graphics;
 
     opens frontend.application to javafx.fxml;
     exports frontend.application;
