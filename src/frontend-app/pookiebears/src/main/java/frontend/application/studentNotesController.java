72b2094e60fdabb5363aaf036cc2c3a6f273cdf7
Victor Nguyen <vicvngu@gmail.com>
2024-04-21 23:28:02 -0400

    commit fixing

--- src/frontend-app/pookiebears/src/main/java/frontend/application/studentNotesController.java
+++ src/frontend-app/pookiebears/src/main/java/frontend/application/studentNotesController.java
@@ -9,30 +9,13 @@ import backend.Student;
 import backend.UserList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-<<<<<<< HEAD
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
-=======
-import javafx.scene.control.TextArea;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Text;
-
-
->>>>>>> 5e4df092a20bc022e25e3034d7119fc89c040c1f
 
 public class studentNotesController implements Initializable{
-
-    
-
-    @FXML
-    private Text notesText;
-
-
-    private RoadmapApplication application;
-
 
     @FXML
     void academicClicked(MouseEvent event) throws IOException{
@@ -52,7 +35,6 @@ public class studentNotesController implements Initializable{
 
     }
 
-<<<<<<< HEAD
     @FXML
     private TextArea textField;
     
@@ -60,14 +42,10 @@ public class studentNotesController implements Initializable{
 
     @FXML
     private Text notes;
-=======
-
->>>>>>> 5e4df092a20bc022e25e3034d7119fc89c040c1f
 
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-<<<<<<< HEAD
         application = RoadmapApplication.getInstance();
         UserList users = UserList.getInstance();
         Student student = (Student)users.getActive();
@@ -89,17 +67,6 @@ public class studentNotesController implements Initializable{
         }
 
 
-=======
-
-       application = RoadmapApplication.getInstance();
-
-       UserList users = UserList.getInstance();
-       Student student = (Student)users.getActive();
-
-       String notes = student.getNote();
-       
-       notesText.setText(notes);
->>>>>>> 5e4df092a20bc022e25e3034d7119fc89c040c1f
         
 
 
